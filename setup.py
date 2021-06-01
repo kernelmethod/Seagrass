a08b6302c6b43508eff9ba1c6ce39e5f1cb89141
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2021-06-01 01:56:31 -0600

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="seagrass",
+    version="0.1.0",
+    description="Auditing and profiling multi-tool",
+    author="kernelmethod",
+    author_email="17100608+kernelmethod@users.noreply.github.com",
+    url="https://github.com/kernelmethod/Seagrass",
+    packages=["seagrass"],
+    install_requires=[],
+)
