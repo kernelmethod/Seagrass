75fa3bf4f410893a26e9139157eb9329f4982646
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2021-06-01 18:44:17 -0600

    Add an auditing hook for collecting stack traces.

--- seagrass/hooks/__init__.py
+++ seagrass/hooks/__init__.py
@@ -1,8 +1,10 @@
 # flake8: noqa: F401
 from .base import ProtoHook
 from .counter_hook import CounterHook
+from .stack_trace_hook import StackTraceHook
 
 __all__ = [
     "CounterHook",
     "ProtoHook",
+    "StackTraceHook",
 ]
