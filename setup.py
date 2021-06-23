df5d5e7d284cc456ce63ca795caef7de78c7c151
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2021-06-22 19:19:50 -0600

    Bump version to 0.4.2.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="seagrass",
-    version="0.4.1",
+    version="0.4.2",
     description="Auditing and profiling multi-tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
